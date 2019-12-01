7cf0c0aaee86d401b6fd8700148871c7c3a85f0b
MaciejDebskiLd <maciej.debski@yahoo.com>
2019-12-01 09:22:37 +0100

    some fixes

--- src/main/java/com/example/keycloak2/config/KeycloakConfig.java
+++ src/main/java/com/example/keycloak2/config/KeycloakConfig.java
@@ -22,19 +22,16 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 public class KeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-                = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-                new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    public void configureGlobal (AuthenticationManagerBuilder authenticationManagerBuilder) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        authenticationManagerBuilder.authenticationProvider(keycloakAuthenticationProvider);
     }
 
     @Bean
     @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy(){
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Override
