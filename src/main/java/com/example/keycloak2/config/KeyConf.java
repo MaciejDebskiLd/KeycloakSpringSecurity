2698e305a70eeb685f3a1683b9acb74a141adae4
MaciejDebskiLd <maciej.debski@yahoo.com>
2019-12-01 09:09:36 +0100

    some fixes

--- src/main/java/com/example/keycloak2/config/KeyConf.java
+++ src/main/java/com/example/keycloak2/config/KeyConf.java
@@ -1,9 +1,7 @@
 package com.example.keycloak2.config;
 
 
-import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KeyConf {
     @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
-
     }
 }
